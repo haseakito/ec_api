0c1f630a1e1f1d75472e3dae13e52d0a87b38450
haseakito <haseakito@gmail.com>
2024-02-20 21:14:06 +1100

    :zap: Add functions related with `Review` for a product

--- src/controllers/product_controller.go
+++ src/controllers/product_controller.go
@@ -124,9 +124,8 @@ func (pc ProductController) UpdateProduct(c echo.Context) error {
 	if req.Price != 0 {
 		product.Price = &req.Price
 	}
-	if req.Published != false {
-		product.Published = req.Published
-	}
+
+	product.Published = req.Published
 
 	// Update product with data
 	// If the update is unsuccessful, then throw an error
@@ -303,7 +302,7 @@ func (pc ProductController) DeleteProductImage(c echo.Context) error {
 	// Get a product image with product id and product image id
 	// If there is no record, then throw a NotFound error
 	var productImage models.ProductImage
-	res := pc.db.First(&productImage, "id = ? AND product_id = ?", productImageId, productId)
+	res := pc.db.Find(&productImage, "id = ? AND product_id = ?", productImageId, productId)
 
 	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
 		c.JSON(http.StatusNotFound, nil)
@@ -323,4 +322,143 @@ func (pc ProductController) DeleteProductImage(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, "Successfully deleted the product image")
+}
+
+/*
+Description:
+
+	Create a review for a specific product with the product id and based on the data provided in the request payload.
+
+HTTP Method:
+
+	POST `/api/v1/products/:id/reviews`
+
+Parameters:
+
+	c (echo.Context): Context object containing the HTTP request information.
+
+Returns:
+
+	An error if any occurred during the execution of the function, nil otherwise.
+*/
+func (pc ProductController) CreateReview(c echo.Context) error {
+	// Get product id from request
+	productId := c.Param("id")
+
+	// Get a store with store id
+	var product models.Product
+	res := pc.db.Take(&product, "id = ?", productId)
+
+	// If there is no record, then throw a NotFound error
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, nil)
+		return nil
+	}
+
+	// Parsing request payload and validate the data
+	// If there is a problem with the request, throw an error
+	var req requests.ReviewCreateRequest
+	if err := c.Bind(&req); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return nil
+	}
+
+	// Validate request data
+	// If there is a problem with the request, throw an error
+	if err := req.Validate(); err != nil {
+		c.JSON(http.StatusBadRequest, err)
+		return nil
+	}
+
+	// Instantiate a new review
+	review := models.Review{
+		ProductID: productId,
+		UserID:    req.UserID,
+		Content:   req.Content,
+	}
+
+	// Create a new review for the product
+	if res := pc.db.Create(&review); res.Error != nil {
+		c.JSON(http.StatusInternalServerError, res.Error)
+		return nil
+	}
+
+	return c.JSON(http.StatusCreated, review)
+}
+
+/*
+Description:
+
+	Get all reviews for a specific product with the product id.
+
+HTTP Method:
+
+	GET `/api/v1/product/:id/reviews`
+
+Parameters:
+
+	c (echo.Context): Context object containing the HTTP request information.
+
+Returns:
+
+	An error if any occurred during the execution of the function, nil otherwise.
+*/
+func (pc ProductController) GetReviews(c echo.Context) error {
+	// Get product id from request
+	productId := c.Param("id")
+
+	// Get a product with product id
+	// If there is no record, then throw a NotFound error
+	var reviews []models.Review
+	res := pc.db.Where("product_id = ?", productId).Find(&reviews)
+
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, nil)
+		return nil
+	}
+
+	return c.JSON(http.StatusOK, reviews)
+}
+
+/*
+Description:
+
+	Delete a specific review with the review id.
+
+HTTP Method:
+
+	DELETE `/api/v1/products/:id/reviews/:review_id`
+
+Parameters:
+
+	c (echo.Context): Context object containing the HTTP request information.
+
+Returns:
+
+	An error if any occurred during the execution of the function, nil otherwise.
+*/
+func (pc ProductController) DeleteReview(c echo.Context) error {
+	// Get product id from request
+	productId := c.Param("id")
+
+	// Get review id from request
+	reviewId := c.Param("review_id")
+
+	// Get a product image with product id and product image id
+	// If there is no record, then throw a NotFound error
+	var review models.Review
+	res := pc.db.Find(&review, "id = ? AND product_id = ?", reviewId, productId)
+
+	if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+		c.JSON(http.StatusNotFound, nil)
+		return nil
+	}
+
+	// Delete the review record
+	if res := pc.db.Delete(&review); res.Error != nil {
+		c.JSON(http.StatusInternalServerError, res.Error)
+		return nil
+	}
+
+	return c.JSON(http.StatusOK, "Successfully deleted the review")
 }
